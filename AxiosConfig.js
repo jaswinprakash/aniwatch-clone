7aca21556aa441ec2e21cd1ec86075d25e77f796
jaswinprakash.talrop <jaswinprakash.talrop@gmail.com>
2025-03-20 12:23:21 +0530

    minor axios changes

--- AxiosConfig.js
+++ AxiosConfig.js
@@ -2,4 +2,7 @@ import axios from "axios";
 
 export const apiConfig = axios.create({
     baseURL: "https://demo-aniwatch.vercel.app",
+    headers: {
+        Referer: 'https://megacloud.club/',
+    },
 });
