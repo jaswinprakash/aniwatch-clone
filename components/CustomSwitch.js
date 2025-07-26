6583f137855024160473cf7e396b2a782deb7823
jaswinprakash.talrop <jaswinprakash.talrop@gmail.com>
2025-07-26 18:46:39 +0530

    webview button animation fixes

--- components/CustomSwitch.js
+++ components/CustomSwitch.js
@@ -1,10 +1,11 @@
 import React from "react";
-import {
-    Animated,
-    StyleSheet,
-    TouchableWithoutFeedback,
-    View,
-} from "react-native";
+import { StyleSheet, TouchableWithoutFeedback, View } from "react-native";
+import Animated, {
+    interpolateColor,
+    useAnimatedStyle,
+    useSharedValue,
+    withTiming,
+} from "react-native-reanimated";
 import { Colors } from "../constants/Colors";
 import { SIZE } from "../constants/Constants";
 
@@ -16,49 +17,77 @@ const CustomSwitch = ({ value, onValueChange }) => {
     const thumbColor = value ? Colors.light.tabIconSelected : "#fff";
     const iosBackgroundColor = "#3e3e3e";
 
-    const [animatedValue] = React.useState(new Animated.Value(value ? 1 : 0));
+    // Pre-calculate all SIZE values
+    const sizeValues = {
+        trackWidth: SIZE(40),
+        thumbSize: SIZE(18),
+        thumbRadius: SIZE(9),
+        thumbStart: SIZE(1),
+        thumbEnd: SIZE(19),
+        trackHeight: SIZE(20),
+        trackRadius: SIZE(10),
+        containerHeight: SIZE(24),
+        shadowOffset: SIZE(1),
+        shadowRadius: SIZE(1.5),
+        thumbTop: SIZE(1),
+    };
+
+    const progress = useSharedValue(value ? 1 : 0);
 
     React.useEffect(() => {
-        Animated.timing(animatedValue, {
-            toValue: value ? 1 : 0,
-            duration: 200,
-            useNativeDriver: false,
-        }).start();
-    }, [value]);
+        progress.value = withTiming(value ? 1 : 0, { duration: 200 });
+    }, [value, progress]);
 
-    const thumbPosition = animatedValue.interpolate({
-        inputRange: [0, 1],
-        outputRange: [SIZE(1), SIZE(19)],
+    const thumbStyle = useAnimatedStyle(() => {
+        "worklet";
+        return {
+            transform: [
+                {
+                    translateX:
+                        sizeValues.thumbStart +
+                        progress.value *
+                            (sizeValues.thumbEnd - sizeValues.thumbStart),
+                },
+            ],
+            backgroundColor: thumbColor,
+        };
     });
 
-    const trackWidth = animatedValue.interpolate({
-        inputRange: [0, 1],
-        outputRange: [SIZE(40), SIZE(40)],
-    });
-
-    const trackBackgroundColor = animatedValue.interpolate({
-        inputRange: [0, 1],
-        outputRange: [iosBackgroundColor, trackColor.true],
+    const trackStyle = useAnimatedStyle(() => {
+        "worklet";
+        return {
+            width: sizeValues.trackWidth,
+            height: sizeValues.trackHeight,
+            borderRadius: sizeValues.trackRadius,
+            backgroundColor: interpolateColor(
+                progress.value,
+                [0, 1],
+                [iosBackgroundColor, trackColor.true]
+            ),
+        };
     });
 
     return (
         <TouchableWithoutFeedback onPress={() => onValueChange(!value)}>
-            <View style={styles.container}>
-                <Animated.View
-                    style={[
-                        styles.track,
-                        {
-                            width: trackWidth,
-                            backgroundColor: trackBackgroundColor,
-                        },
-                    ]}
-                >
+            <View
+                style={[
+                    styles.container,
+                    {
+                        height: sizeValues.containerHeight,
+                        width: sizeValues.trackWidth,
+                    },
+                ]}
+            >
+                <Animated.View style={[styles.track, trackStyle]}>
                     <Animated.View
                         style={[
                             styles.thumb,
+                            thumbStyle,
                             {
-                                backgroundColor: thumbColor,
-                                transform: [{ translateX: thumbPosition }],
+                                width: sizeValues.thumbSize,
+                                height: sizeValues.thumbSize,
+                                borderRadius: sizeValues.thumbRadius,
+                                top: sizeValues.thumbTop,
                             },
                         ]}
                     />
@@ -70,28 +99,18 @@ const CustomSwitch = ({ value, onValueChange }) => {
 
 const styles = StyleSheet.create({
     container: {
-        height: SIZE(24),
-        width: SIZE(40),
         justifyContent: "center",
         alignItems: "center",
     },
     track: {
-        height: SIZE(20),
-        borderRadius: SIZE(10),
-        backgroundColor: "#3e3e3e",
+        // Dimensions moved to animated style
     },
     thumb: {
         position: "absolute",
-        width: SIZE(18),
-        height: SIZE(18),
-        borderRadius: SIZE(9),
-        backgroundColor: "#fff",
         shadowColor: "#000",
-        shadowOffset: { width: 0, height: SIZE(1) },
+        shadowOffset: { width: 0, height: 1 },
         shadowOpacity: 0.2,
-        shadowRadius: SIZE(1.5),
         elevation: 2,
-        top: SIZE(1),
     },
 });
 
